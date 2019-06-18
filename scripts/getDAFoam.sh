df3e0d287c11a86f064224e70426ed9a8183e72d
Ping He <friedenhe@gmail.com>
2019-06-17 20:05:12 -0400

    Uploaded files.

---
+++ scripts/getDAFoam.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a2b2555552de0d510464b7e209c6acec06fcafdc2e7bea8c11bd48cae978b3da
+size 1553
---
+++ scripts/startDAFoam.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d3570ab54eeafb0170bf27fed841d8eb080a62ab30f7335f28e7cf3b46e14abe
+size 81
